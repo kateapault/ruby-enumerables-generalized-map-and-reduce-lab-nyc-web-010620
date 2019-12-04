099a60b64530b6514f32649e613d38609cf3a013
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 21:23:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,12 @@
 # Your Code Here
 
 def map(source_array)
+  out_array = []
   for each in source_array do
-    yield(each)
+    out_value = yield(each)
+    out_array << out_array
   end
+  out_array
 end
 
 def reduce(source_array, starting_point = 0)
