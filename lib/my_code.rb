2d9c09238a0e444b915ab1ff3eaf5fe6c35c9047
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 21:19:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,17 @@
 # Your Code Here
 
+def map(source_array, block)
+  def dry_map(source_array)
+    for each in source_array do
+      yield(each)
+    end
+  end
+  
+  output = dry_map(source_array) block
+  output
+end
+
+def reduce(source_array, starting_point = 0)
+  x = starting_point
+  
+end
